1d9f4cd2f119dc94ce6cd5b2266a96b942cecc2d
Milos Turek <milos.turek@gmail.com>
2018-05-06 21:45:27 +0200

    Fixed images

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ export const Header = ({ title, navigation }) => (
     <React.Fragment>
         <header>
             <div className="siteHeaderImage">
-                <Link to="/"><img src="img/logo_black.png" /></Link>
+                <Link to="/"><img src="/img/logo_black.png" /></Link>
             </div>
             <div className="siteHeader">
                 <Link to="/">{title}</Link>
@@ -17,7 +17,7 @@ export const Header = ({ title, navigation }) => (
             </nav>
         </header>
         <div className="topImage">
-            <img src="img/uvodnivsichni_mod.jpg" />
+            <img src="/img/uvodnivsichni_mod.jpg" />
         </div>
     </React.Fragment>
 );
