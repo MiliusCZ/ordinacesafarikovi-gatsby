2e1ab71a0dd19fe29792264b73cb76b5c50fead2
Milos Turek <milos.turek@gmail.com>
2018-05-06 22:28:31 +0200

    Added services page

--- src/pages/o-nas.js
+++ src/pages/o-nas.js
@@ -3,7 +3,7 @@ import React from "react"
 export default ({ data }) => (
     <React.Fragment>
         <h1>{data.allMarkdownRemark.edges[0].node.frontmatter.title}</h1>
-        <div dangerouslySetInnerHTML={{ __html: data.allMarkdownRemark.edges[0].node.content }} />
+        <div dangerouslySetInnerHTML={{ __html: data.allMarkdownRemark.edges[0].node.html }} />
     </React.Fragment>
 );
 
--- src/pages/sluzby.js
+++ src/pages/sluzby.js
@@ -1,3 +1,26 @@
 import React from "react"
 
-export default () => <div>Sluzby</div>
+export default ({ data }) => (
+    <React.Fragment>
+        <h1>{data.allMarkdownRemark.edges[0].node.frontmatter.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: data.allMarkdownRemark.edges[0].node.html }} />
+    </React.Fragment>
+);
+
+export const query = graphql`
+  query ServicesQuery {
+    allMarkdownRemark (
+      filter: { frontmatter: { key: { eq: "services" }}}
+    ){
+      edges {
+        node {
+          frontmatter {
+            title
+            key
+          }
+          html
+        }
+      }
+    }
+  }
+`;
