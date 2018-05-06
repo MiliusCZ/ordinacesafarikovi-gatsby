7ade0617329bd76db4917474a83d278eac024b32
Milos Turek <milos.turek@gmail.com>
2018-05-06 22:11:58 +0200

    Added md query

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,7 @@ module.exports = {
     plugins: [
         'gatsby-plugin-sass',
         'gatsby-transformer-json',
+        'gatsby-transformer-remark',
         {
             resolve: 'gatsby-source-filesystem',
             options: {
--- src/pages/o-nas.js
+++ src/pages/o-nas.js
@@ -2,25 +2,25 @@ import React from "react"
 
 export default ({ data }) => (
     <React.Fragment>
-        <h1>{data.allContentJson.edges[0].node.title}</h1>
-        <div>{data.allContentJson.edges[0].node.content}</div>
+        <h1>{data.allContentJson.edges[0].node.frontmatter.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: data.allContentJson.edges[0].node.content }} />
     </React.Fragment>
 );
 
 export const query = graphql`
   query AboutUsQuery {
-	  allContentJson (    
-      filter: {
-        key: {eq: "about-us"}
+    allMarkdownRemark (
+      filter: { frontmatter: { key: { eq: "about-us" }}}
+    ){
+      edges {
+        node {
+          frontmatter {
+            title
+            key
+          }
+          html
+        }
       }
-    ) {
-  	edges {
-    	node {
-        key
-	      title
-        content
-  	    }
-  	  }
     }
   }
 `;
