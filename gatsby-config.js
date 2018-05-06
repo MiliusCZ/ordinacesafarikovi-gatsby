c99818d15fae31d809891fa80a43c3629cdb4b99
Milos Turek <milos.turek@gmail.com>
2018-05-06 21:40:29 +0200

    Editable content page

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,13 @@ module.exports = {
             },
         },
         {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+              path: `${__dirname}/src/content`,
+              name: 'content'
+            },
+        },
+        {
             resolve: 'gatsby-plugin-typography',
             options: {
               pathToConfigModule: 'src/utils/typography.js',
--- src/pages/o-nas.js
+++ src/pages/o-nas.js
@@ -1,3 +1,26 @@
 import React from "react"
 
-export default () => <div>O nas</div>
+export default ({ data }) => (
+    <React.Fragment>
+        <h1>{data.allContentJson.edges[0].node.title}</h1>
+        <div>{data.allContentJson.edges[0].node.content}</div>
+    </React.Fragment>
+);
+
+export const query = graphql`
+  query AboutUsQuery {
+	  allContentJson (    
+      filter: {
+        key: {eq: "about-us"}
+      }
+    ) {
+  	edges {
+    	node {
+        key
+	      title
+        content
+  	    }
+  	  }
+    }
+  }
+`;
