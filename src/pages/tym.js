6c01208ae96b7263a1926a4d6836838ad2452f4e
Milos Turek <milos.turek@gmail.com>
2018-10-28 12:57:46 +0100

    added team logic, show in menu

--- src/pages/tym.js
+++ src/pages/tym.js
@@ -1,4 +1,40 @@
 import React from "react";
 import Layout from "../components/Layout";
 
-export default () => <Layout>Tym</Layout>
+import { graphql } from 'gatsby';
+
+export default ({ data }) => {
+    const teamMembers = data.allContentJson.edges[0].node.teamMembers;
+
+    const teamMembersDisplay = teamMembers.map((teamMember) => <TeamMebmer data={teamMember} key={teamMember.name} />);
+
+    return (
+        <Layout>{teamMembersDisplay}</Layout>
+    )
+}
+
+const TeamMebmer = ({ data }) => (
+    <>
+        <h2>{data.name}</h2>
+        <span>{data.specialization}</span>
+        <img src={data.photo} alt={data.name} />
+        <p dangerouslySetInnerHTML={{ __html: data.bio }} />
+    </>
+);
+
+export const query = graphql`
+  query TeamQuery {
+    allContentJson {
+      edges {
+        node {
+          teamMembers {
+            name
+            photo
+            specialization
+            bio
+          }
+        }
+      }
+    }
+  }
+`;
