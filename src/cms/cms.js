d4d58936270c437189e04a5d87306baef766f117
Milos Turek <milos.turek@gmail.com>
2018-10-28 18:18:41 +0100

    implemented member control for preview

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,13 +1,20 @@
 import "netlify-cms/dist/cms.css";
 import React from "react";
 
+import { TeamMember } from "../components/TeamMember";
+
 import CMS from 'netlify-cms';
 
 const TeamMebmerPreview = ({ widgetsFor }) => (
   <div>
-      {widgetsFor('teamMembers').map(member => (
-        <div><h3>{member.getIn(['data', 'name'])}</h3></div>
-      ))}
+      {widgetsFor('teamMembers').map(member => {(
+        const memberData = {
+          name: member.getIn(['data', 'name']),
+          photo: member.getIn(['data', 'photo']),
+          specialization: member.getIn(['data', 'specialization'])
+        };
+        return (<TeamMember data={memberData} key={memberData.name}/>)
+      )})}
   </div>  
 );
 
