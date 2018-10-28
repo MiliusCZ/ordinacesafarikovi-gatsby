913219a0ffb76d5c7b5e5c73778cfa5bc441b061
Milos Turek <milos.turek@gmail.com>
2018-10-28 18:23:15 +0100

    fixed parenthesis bugs

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -7,14 +7,14 @@ import CMS from 'netlify-cms';
 
 const TeamMebmerPreview = ({ widgetsFor }) => (
   <div>
-      {widgetsFor('teamMembers').map(member => {(
+      {widgetsFor('teamMembers').map(member => {
         const memberData = {
           name: member.getIn(['data', 'name']),
           photo: member.getIn(['data', 'photo']),
           specialization: member.getIn(['data', 'specialization'])
         };
-        return (<TeamMember data={memberData} key={memberData.name}/>)
-      )})}
+        return (<TeamMember data={memberData} key={memberData.name}/>);
+      })}
   </div>  
 );
 
