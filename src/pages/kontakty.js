b1be80b83b4c1586c08a231051e48d707a8504fe
Milos Turek <milos.turek@gmail.com>
2018-10-28 18:42:33 +0100

    added contacts page

--- src/pages/kontakty.js
+++ src/pages/kontakty.js
@@ -1,4 +1,27 @@
 import React from "react";
 import Layout from "../components/Layout";
 
-export default () => <Layout>Kontakty</Layout>
+export default ({ data }) => (
+    <Layout>
+        <h1>{data.allMarkdownRemark.edges[0].node.frontmatter.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: data.allMarkdownRemark.edges[0].node.html }} />
+    </Layout>
+);
+
+export const query = graphql`
+  query ContactsQuery {
+    allMarkdownRemark (
+      filter: { frontmatter: { key: { eq: "contacts" }}}
+    ){
+      edges {
+        node {
+          frontmatter {
+            title
+            key
+          }
+          html
+        }
+      }
+    }
+  }
+`;
