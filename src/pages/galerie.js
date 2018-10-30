bb70620276ce76db2dae38047584c46c98c3fabb
Milos Turek <milos.turek@gmail.com>
2018-10-30 21:24:05 +0100

    added gallery page

--- src/pages/galerie.js
+++ src/pages/galerie.js
@@ -1,4 +1,43 @@
-import React from "react"
+import React from "react";
 import Layout from "../components/Layout";
+import Gallery from 'react-photo-gallery';
 
-export default () => <Layout>Galerie</Layout>
+import { graphql } from 'gatsby';
+
+export default ({ data }) => {
+    const title = data.allContentJson.edges[0].node.title;
+    const images = data.allContentJson.edges[0].node.galleryImages;
+
+    console.log(images);
+
+    return (
+        <Layout>
+            <h1>{title}</h1>
+            {images && <Gallery photos={images.map((image) => {
+                return {
+                    key: image,
+                    src: image,
+                    alt: image,
+                    width: 3,
+                    height: 2
+                }
+            })} />
+            }
+        </Layout>
+    )
+}
+
+export const query = graphql`
+  query GalleryQuery {
+	  allContentJson(
+      filter: { key: { eq: "gallery" }}
+    ) {
+        edges {
+          node {
+            title
+            galleryImages
+          }
+        }
+      }
+    }
+`;
