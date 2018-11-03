974c2564fce7d54b34dfeb213c678565c0300baf
turekmi <milos.turek@merck.com>
2018-11-03 22:22:01 +0100

    Fixed news component

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export const NewsItem = ({ title, body }) => (
-  <>'   '<h2>{title}</h2>'   '<div dangerouslySetInnerHTML={{ __html: body }} />' '</>
+  <React.Fragment>
+    <h2>{title}</h2>
+    <div dangerouslySetInnerHTML={{ __html: body }} />
+  </React.Fragment>
 );
 
 NewsItem.displayName = 'NewsItem';
 
 NewsItem.propTypes = {
   title: PropTypes.string,
-  bode: PropTypes.string
+  body: PropTypes.string
 };
